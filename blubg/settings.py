7d8d715fc6f5543b8e424a951578a2f1b52423cc
Gustavo Andrés Angulo <woakas@gmail.com>
2011-08-02 07:23:46 -0500

    Added the admin application in the settings.py and admin.py in the blog application

--- blubg/settings.py
+++ blubg/settings.py
@@ -115,8 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'blog'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
